e42ab1997fa4467f65352254faf6aaf301befe5b
hussein <httm010@gmail.com>
2024-07-20 12:54:29 +0300

    kassab branch push

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,32 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
-console.log('url::' , process.env.MONGO_ATLAS)
+console.log('url::', process.env.MONGO_ATLAS);
 
-mongoose.connect(process.env.MONGO_ATLAS).then
-( 
-    () => console.log("connected to db!")
-
-).catch((err) => {
-
+mongoose
+  .connect(process.env.MONGO_ATLAS)
+  .then(() => console.log('connected to db!'))
+  .catch((err) => {
     console.log(err.message);
-})
-
+  });
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/users', userRouter);
 
-app.use('/api/users' , userRouter);
-
-
-
-
-
-app.use((err , req , res , next) => {
-    res.status(500).send({message: err.message});
-})
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 
-app.listen(port , () => {
-    console.log(`Listen on port ${port}!!`);
-})
-
+app.listen(port, () => {
+  console.log(`Listen on port ${port}!!`);
+});
