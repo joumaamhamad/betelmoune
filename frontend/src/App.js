99bd072e2eda6df6fae11838cf75c587f16bf34e
Mohammad-Khalifeh <mohammad_khalifeh.1@hotmail.com>
2024-07-20 12:45:29 +0300

    npm start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './screens/HomePage';
 import SignIn from './screens/Signin';
 
-
 function App() {
   return (
     <BrowserRouter>
       <div className="App flex flex-col min-h-screen">
-        <header className="bg-gray-800 text-white p-4">
-          headerr
-        </header>
+        <header className="bg-gray-800 text-white p-4">headerr</header>
 
         <main className="flex-grow">
           <Routes>
-            <Route path='/' element={<HomePage />} />
+            <Route path="/" element={<HomePage />} />
           </Routes>
         </main>
 
-        <footer className="bg-gray-800 text-white p-4">
-          footer
-        </footer>
+        <footer className="bg-gray-800 text-white p-4">footer</footer>
       </div>
     </BrowserRouter>
   );
 }
 
 export default App;
-
