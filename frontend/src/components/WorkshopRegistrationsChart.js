02be5b71ccc868b86b901889f76e79e96e2d8c02
Mhamad jomaa <145396495+joumaamhamad@users.noreply.github.com>
2024-09-07 17:18:52 +0300

    fix admin dashborad

--- frontend/src/components/WorkshopRegistrationsChart.js
+++ frontend/src/components/WorkshopRegistrationsChart.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
 import axios from 'axios';
 
-import { t } from 'i18next';
-
-
 const WorkshopRegistrationsChart = () => {
   const [registrationData, setRegistrationData] = useState([]);
 
@@ -13,9 +10,7 @@ const WorkshopRegistrationsChart = () => {
       try {
         const { data } = await axios.get('api/charts/workshopRegistrations');
         setRegistrationData(data.data);
-
-        console.log('dddddddddd', data);
-
+        console.log('dddddddddd',data)
       } catch (error) {
         console.log('Error fetching registration data:', error);
       }
@@ -24,13 +19,11 @@ const WorkshopRegistrationsChart = () => {
   }, []);
 
   const chartData = {
-
-    labels: registrationData.map((data) => data.workshopName),
+    labels: registrationData.map(data => data.workshopName),
     datasets: [
       {
-        label: t('Registrations'),
-        data: registrationData.map((data) => data.registrations),
-
+        label: 'Registrations',
+        data: registrationData.map(data => data.registrations),
         backgroundColor: 'rgba(75, 192, 192, 0.6)',
       },
     ],
